644346f860315a4068e7b333423537ff7416c3f7
powerful-room2 <hanhoon12@naver.com>
2026-07-15 22:47:06 +0900

    Change build dir

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: { port: 5173 },
-  build: { target: 'es2022' },
+  build: { 
+    target: 'es2022',
+    outDir: 'docs'
+  },
 });
